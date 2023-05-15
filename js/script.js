6948447aa00371aa891f380976c3a8cbfc405e69
pawelok <pawelok35@gmail.com>
2023-05-15 20:17:32 +0200

    update script.js by the rest exercises

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 
 const titleClickHandler = function (event) {
   console.log("Link was clicked!");
-
+  event.preventDefault();
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll(".titles a.active");
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   const clickedElement = this;
   clickedElement.classList.add("active");
   console.log("clickedElement:", clickedElement);
@@ -22,11 +22,18 @@ const titleClickHandler = function (event) {
   }
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(".posts article");
+  console.log(targetArticle);
+  
   /* add class 'active' to the correct article */
-};
+  targetArticle.classList.add("active");
+  console.log("targetArticle:", targetArticle);
 
+};
 const links = document.querySelectorAll(".titles a");
 
 for (let link of links) {
