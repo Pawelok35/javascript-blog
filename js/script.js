5296408f1f225f9b78704b2ba258903d8e812233
pawelok <pawelok35@gmail.com>
2023-05-16 20:26:17 +0200

    Fixed find the correct article using the selector (value of 'href' attribute)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const titleClickHandler = function (event) {
+const titleClickHandler = function (event) {   /// wyrazenie funkcyjne const title...
   console.log("Link was clicked!");
   event.preventDefault();
   /* [DONE] remove class 'active' from all article links  */
@@ -10,23 +10,23 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove("active");
   }
   /* [DONE] add class 'active' to the clicked link */
-  const clickedElement = this;
+  const clickedElement = this; ///obiekt ktory wywoluje funkcje, klikniety elemnet na ktorym zaszlo zdarzenie
   clickedElement.classList.add("active");
   console.log("clickedElement:", clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts article.active");
 
-  for (let activeArticle of activeArticles) {
+  for (let activeArticle of activeArticles) { /// iteruje po wszytskich elementach
     activeArticle.classList.remove("active");
   }
   /* get 'href' attribute from the clicked link */
 
-  const articleSelector = clickedElement.getAttribute("href");
+  const articleSelector = clickedElement.getAttribute("href"); /// #article-2 #element o id
   console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(".posts article");
+  const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
   
   /* add class 'active' to the correct article */
